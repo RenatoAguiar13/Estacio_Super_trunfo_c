e60912c8f5fc2296c4e904ac1a9e68c30c01f07c
Renato Aguiar <109884233+RenatoAguiar13@users.noreply.github.com>
2026-01-13 01:56:49 +0000

    primeiro commit

---
+++ super_trunfo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (){
+
+    char estado, carta_codigo[20], nome_cidade[20];
+    int populacao, pontos_turisticos;
+    float area, pib;
+
+    printf("Escreva o nome do Estado da primeira carta:\n");
+    fgets(estado,20,stdin);
+
+}
